379e44bf89fa7f19d91faccb9fa02842ad3bab9d
Koth <koth@example.com>
2023-06-26 22:44:13 +0800

    marketing detection as binary classification

--- distill/marketing_detection.py
+++ distill/marketing_detection.py
@@ -15,7 +15,7 @@ class MarketingDetectionModel(nn.Module):
   vocab_size: int
   hidden_size: int
   @nn.compact
-  def __call__(self, x):
+  def __call__(self, x, training=False):
     x = nn.Embed(num_embeddings=self.vocab_size, features=self.hidden_size)(x)
     # now [batch, seqlen, hidden_size]
 
@@ -31,16 +31,17 @@ class MarketingDetectionModel(nn.Module):
     x2 = nn.Conv(features=32, kernel_size=(7,),padding='VALID')(x2)
     x3 = nn.Conv(features=32, kernel_size=(5,),padding='VALID')(x3)
     x4 = nn.Conv(features=32, kernel_size=(3,),padding='VALID')(x4)
-    x1 = nn.gelu(x1)
-    x2 = nn.gelu(x2)
-    x3 = nn.gelu(x3)
-    x4 = nn.gelu(x4)
+    x1 = nn.elu(x1)
+    x2 = nn.elu(x2)
+    x3 = nn.elu(x3)
+    x4 = nn.elu(x4)
     x= jnp.concatenate([x1,x2,x3,x4], axis=2)
     x = nn.avg_pool(x, window_shape=(2,), strides=(2,))
 
     x = x.reshape((x.shape[0], -1))  # flatten
-    x = nn.Dense(features=256)(x)
-    x = nn.relu(x)
+    x = nn.Dropout(rate=0.25)(x,deterministic=not training)
+    x = nn.Dense(features=512)(x)
+    x = nn.gelu(x)
     x = nn.Dense(features=1)(x)
     return x
 
@@ -60,31 +61,29 @@ def lower() -> Lowered:
 
   @jax.jit
   def serving(params,x):
-    return model.apply(params, x)
+    return model.apply(params, x, False)
 
   @jax.jit
   def batchServing(params,x):
-    return model.apply(params, x)
+    return model.apply(params, x, False)
   
 
 
   @jax.jit
   def optimizer_step(params, opt_state, x,y):
-
     def fwd(params):
-      logits = model.apply(params, x)
-      return jnp.abs(logits - y).mean()
+      logits = model.apply(params, x, True,rngs={'dropout': jax.random.PRNGKey(42)})
+      return optax.sigmoid_binary_cross_entropy(logits,y).mean()
       
 
-    grads = jax.grad(fwd)(params)
-    loss = fwd(params)
+    loss,grads = jax.value_and_grad(fwd)(params)
     updates, opt_state = tx.update(grads, opt_state)
     params = optax.apply_updates(params, updates)
     return params, opt_state, loss
 
   params, opt_state = jax.eval_shape(init)
   optimizer_step_lowered = optimizer_step.lower(
-      params, opt_state, jax.ShapeDtypeStruct((128, 2048), jnp.int32),jax.ShapeDtypeStruct((128, 1), jnp.float32))
+      params, opt_state, jax.ShapeDtypeStruct((128, 2048), jnp.int32),jax.ShapeDtypeStruct((128, 1), jnp.int32))
   
   serving_lowered = serving.lower(
       params, jax.ShapeDtypeStruct((1, 2048), jnp.int32))
--- distill/train_marketing_detection.cc
+++ distill/train_marketing_detection.cc
@@ -9,10 +9,10 @@
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "absl/log/check.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
-#include "absl/log/check.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
@@ -25,8 +25,7 @@ ABSL_FLAG(std::string, train_data_path, "data/train.txt", "training data path");
 ABSL_FLAG(std::string, test_data_path, "data/test.txt", "the test data path");
 ABSL_FLAG(std::string, tokenizer_path, "vocab/tokme.model",
           "the tokenizer model path");
-ABSL_FLAG(std::string, out_model_path, "md_model.jax",
-          "model output path");
+ABSL_FLAG(std::string, out_model_path, "md_model.jax", "model output path");
 ABSL_FLAG(int, max_epoch, 50, "the maximum epoch number  of training");
 
 const static int kBatchSize = 128;
@@ -34,10 +33,10 @@ const static int kMaxLen = 2048;
 
 static std::vector<int> kInputBuffer(kBatchSize* kMaxLen, 0);
 static std::vector<int> kTrainBuffer(kBatchSize* kMaxLen, 0);
-static std::vector<float> kTargetBuffer(kBatchSize, 0.0);
+static std::vector<int> kTargetBuffer(kBatchSize, 0);
 
 struct TrainEx {
-  float score;
+  int marketing;
   std::vector<int> data;
 };
 absl::StatusOr<std::tuple<aux::PTree, aux::PTree>> Unpack2Tuple(
@@ -67,7 +66,7 @@ static void convertLineToEx(const std::string& line,
   int quality = 0, saleGrade = 0;
   absl::SimpleAtoi(vs[0], &quality);
   absl::SimpleAtoi(vs[1], &saleGrade);
-  ex.score = saleGrade / 100.0;
+  ex.marketing = saleGrade >= 30.0 ? 1 : 0;
   std::vector<std::vector<int>> ids;
   auto status = encoder->encode_as_ids({vs[2]}, &ids);
   if (!status.ok() || ids.empty()) {
@@ -92,16 +91,16 @@ void loadTestDataset(const std::string& path, const vkcom::BaseEncoder* encoder,
   }
 }
 
-void fillTrainData(const std::vector<TrainEx>& inputData){
+void fillTrainData(const std::vector<TrainEx>& inputData) {
   CHECK_EQ(inputData.size(), kBatchSize);
   for (int k = 0; k < kBatchSize; k++) {
-      for (int j = 0; j < inputData[k].data.size(); j++) {
-        kTrainBuffer[k * kMaxLen + j] = inputData[k].data[j];
-      }
-      for (int j = inputData[k].data.size(); j < kMaxLen; j++) {
-        kTrainBuffer[k * kMaxLen + j] = 0;
-      }
-      kTargetBuffer[k]=inputData[k].score;
+    for (int j = 0; j < inputData[k].data.size(); j++) {
+      kTrainBuffer[k * kMaxLen + j] = inputData[k].data[j];
+    }
+    for (int j = inputData[k].data.size(); j < kMaxLen; j++) {
+      kTrainBuffer[k * kMaxLen + j] = 0;
+    }
+    kTargetBuffer[k] = inputData[k].marketing;
   }
 }
 float evalTestDataset(const std::vector<TrainEx>& testData,
@@ -133,26 +132,28 @@ float evalTestDataset(const std::vector<TrainEx>& testData,
     auto resultLiteral = *result.ToArray();
     for (int k = start; k < end; k++) {
       const TrainEx& ex = testData[k];
-      sumDiff += std::abs(resultLiteral.data<float>()[k - start] - ex.score);
+      int predy = resultLiteral.data<float>()[k - start] > 0 ? 1 : 0;
+      sumDiff += predy == ex.marketing ? 1 : 0;
     }
   }
-  LOG(INFO) << "test avg error: " << sumDiff / testData.size();
+  LOG(INFO) << "test accuracy: " << sumDiff / testData.size();
   return sumDiff / testData.size();
 }
-bool saveModel(const std::string& path,const std::vector<const aux::DeviceArray*>& params){
+bool saveModel(const std::string& path,
+               const std::vector<const aux::DeviceArray*>& params) {
   std::ofstream file(path, std::ios::binary);
-  if(!file){
+  if (!file) {
     return false;
   }
-  int numBuf=params.size();
+  int numBuf = params.size();
   file.write(reinterpret_cast<const char*>(&numBuf), sizeof(int));
-  for(int i=0;i<numBuf;i++){
+  for (int i = 0; i < numBuf; i++) {
     const aux::DeviceArray* p = params[i];
-    auto ia=*p->ToArrays();
+    auto ia = *p->ToArrays();
     auto dataArr = ia.data()->data<float>();
     int numData = dataArr.size();
     file.write(reinterpret_cast<const char*>(&numData), sizeof(int));
-    for(int j=0;j<numData;j++){
+    for (int j = 0; j < numData; j++) {
       file.write(reinterpret_cast<const char*>(&dataArr[j]), sizeof(float));
     }
   }
@@ -185,43 +186,47 @@ int main(int argc, char* argv[]) {
 
   auto [params, opt_state] = *Unpack2Tuple(init_fn());
   LOG(INFO) << "inited model , num buffer in weights:" << params.num_buffers();
-  float bestLoss=evalTestDataset(testData, batchServing, dev, params, kMaxLen);
+  float bestAccuracy =
+      evalTestDataset(testData, batchServing, dev, params, kMaxLen);
   std::string line;
   std::vector<TrainEx> trainData;
-  uint32_t step=0;
+  uint32_t step = 0;
   for (int epoch = 0; epoch < maxEpoch; epoch++) {
-    LOG(INFO)<<"begin epoch "<<epoch;
+    LOG(INFO) << "begin epoch " << epoch;
     std::ifstream file(absl::GetFlag(FLAGS_train_data_path));
     aux::PTree lossPT;
     while (std::getline(file, line)) {
       TrainEx ex;
       convertLineToEx(line, encoder.get(), kMaxLen, ex);
       trainData.push_back(ex);
-      if(trainData.size()==kBatchSize){
+      if (trainData.size() == kBatchSize) {
         fillTrainData(trainData);
         auto x = *aux::PTree::BufferRN<int>(absl::Span<int>(kTrainBuffer),
-                                        {kBatchSize, kMaxLen}, dev);
-        auto y = *aux::PTree::BufferRN<float>(absl::Span<float>(kTargetBuffer),
-                                        {kBatchSize,1}, dev);
+                                            {kBatchSize, kMaxLen}, dev);
+        auto y = *aux::PTree::BufferRN<int>(absl::Span<int>(kTargetBuffer),
+                                            {kBatchSize, 1}, dev);
 
-        std::tie(params, opt_state, lossPT) = *Unpack3Tuple(optimizer_step_fn(params, opt_state,x,y));
-        step +=1;
+        std::tie(params, opt_state, lossPT) =
+            *Unpack3Tuple(optimizer_step_fn(params, opt_state, x, y));
+        step += 1;
         auto resultLiteral = *lossPT.ToArray();
-        if(step % 20 ==0){
-          LOG(INFO)<<"Step:"<<step<<",Loss: "<<resultLiteral.data<float>()[0];
-        }                               
+        if (step % 20 == 0) {
+          LOG(INFO) << "Step:" << step
+                    << ",Loss: " << resultLiteral.data<float>()[0];
+        }
         trainData.clear();
       }
     }
-    float loss = evalTestDataset(testData, batchServing, dev, params, kMaxLen);
-    if(loss < bestLoss){
-      bestLoss =loss;
+    float accuracy =
+        evalTestDataset(testData, batchServing, dev, params, kMaxLen);
+    if (accuracy > bestAccuracy) {
+      bestAccuracy = accuracy;
       std::vector<const aux::DeviceArray*> flattened_params;
       CHECK(params.FlattenTo(&flattened_params).ok());
       saveModel(absl::GetFlag(FLAGS_out_model_path), flattened_params);
-      LOG(INFO)<<"new best model"<<bestLoss;
+      LOG(INFO) << "new best model:" << bestAccuracy;
     }
   }
-  LOG(INFO)<<"Finished, best loss:"<<bestLoss;
+  LOG(INFO) << "Finished, best accuracy:" << bestAccuracy;
   return 0;
 }
